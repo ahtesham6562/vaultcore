0d56479c58b091f34691ca8169da54da4e1ab4ef
ahtesham6562 <ahteshamhamidraza@gmail.com>
2026-03-28 12:43:44 +0530

    feat: initial project setup - schema, docker, config

---
+++ backend/src/main/java/vaultcore_backend/VaultcoreBackendApplication.java
@@ -0,0 +1,13 @@
+package vaultcore_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultcoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultcoreBackendApplication.class, args);
+	}
+
+}
